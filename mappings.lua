e6d3bf68b9cd87a88e5795bf5dc7dc5f0c2e2828
te9c <roman.te9c@gmail.com>
2023-05-11 00:57:34 +0500

    add mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -2,6 +2,7 @@ return {
     n = {
         ["o"] = { "o<esc>", desc = "Add new line without entering input mode" },
         ["O"] = { "O<esc>", desc = "Add new line backwards without entering input mode" },
+
         ["<leader>Q"] = { ":qa<cr>", desc = "All quit" }
     }
 }
