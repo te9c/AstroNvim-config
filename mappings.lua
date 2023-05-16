d4f663bb1c659f489e30a481b518e0542f727e60
te9c <roman.te9c@gmail.com>
2023-05-16 18:52:01 +0500

    add mappings

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,8 @@ return {
         ["o"] = { "o<esc>", desc = "Add new line without entering input mode" },
         ["O"] = { "O<esc>", desc = "Add new line backwards without entering input mode" },
 
-        ["<leader>Q"] = { ":qa<cr>", desc = "All quit" }
+        ["<leader>Q"] = { ":qa<cr>", desc = "All quit" },
+
+        ["<leader>dfc"] = { ":Telescope dap configurations<cr>", desc = "Show dap configurations" },
     }
 }
